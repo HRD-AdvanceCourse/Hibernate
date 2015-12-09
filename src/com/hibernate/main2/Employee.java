17d54b5e326354167a0486327ac12f27c1cc5bad
Yin Kokpheng <yinkokpheng@yahoo.com>
2015-12-09 11:40:44 +0700

    new

---
+++ src/com/hibernate/main2/Employee.java
@@ -0,0 +1,92 @@
+package com.hibernate.main2;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="EmployeeInfo")
+public class Employee {
+	
+	@Id @Column(name ="EmployeeID") 	
+	//Create a table for storing pkvalue by mapping empid column
+	//-name=mapping to generator
+	//-table=tablename
+	//-pkColumnName=column name in tablename
+	//-pkColumnValue=store value of empid column
+	//-allocationSize=next size
+	@TableGenerator(name="empid", table="emppktb", pkColumnName="empkey", pkColumnValue="empvalue", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.TABLE, generator="empid")
+	private int empId;
+	private String empName;
+
+	@Transient //This column will not save to table
+	private String empPassword;
+	
+	@Column(nullable=false) //not null
+	private String empEmailAddress;
+	
+	@Basic //Default annotation
+	private boolean isPermanent;
+	
+	@Temporal(TemporalType.DATE) //Use only Date
+	private Calendar empJoinDate;
+	
+	@Temporal(TemporalType.TIMESTAMP) //Use only time
+	private Date empLoginTime;
+	
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpPassword() {
+		return empPassword;
+	}
+	public void setEmpPassword(String empPassword) {
+		this.empPassword = empPassword;
+	}
+	public String getEmpEmailAddress() {
+		return empEmailAddress;
+	}
+	public void setEmpEmailAddress(String empEmailAddress) {
+		this.empEmailAddress = empEmailAddress;
+	}
+	public boolean isPermanent() {
+		return isPermanent;
+	}
+	public void setPermanent(boolean isPermanent) {
+		this.isPermanent = isPermanent;
+	}
+	public Calendar getEmpJoinDate() {
+		return empJoinDate;
+	}
+	public void setEmpJoinDate(Calendar empJoinDate) {
+		this.empJoinDate = empJoinDate;
+	}
+	public Date getEmpLoginTime() {
+		return empLoginTime;
+	}
+	public void setEmpLoginTime(Date empLoginTime) {
+		this.empLoginTime = empLoginTime;
+	}
+}
---
+++ src/com/hibernate/main2/Main.java
@@ -0,0 +1,32 @@
+package com.hibernate.main2;
+
+import java.sql.Date;
+import java.util.GregorianCalendar;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+	
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		
+	
+		emp.setEmpId(100);
+		emp.setEmpName("Kokpheng");
+		emp.setEmpEmailAddress("Yinkokpheng@yahoo.com");
+		emp.setEmpPassword("my password");
+		emp.setPermanent(true);
+		emp.setEmpJoinDate(new GregorianCalendar(2015,05,26));
+		emp.setEmpLoginTime(Date.valueOf("2015-05-26"));
+		
+		session.save(emp);
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+	}
+}
